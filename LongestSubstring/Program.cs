d1bcb656c4097ed6957bcc9e9f04b5a7e804ca7a
smohajer <36665242+smohajer@users.noreply.github.com>
2018-05-19 16:22:41 -0400

    Add project files.

---
+++ LongestSubstring/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstring
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Solution solution = new Solution();
+            Console.WriteLine(solution.LengthOfLongestSubstring("pwwkew"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("abcabcbb"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("au"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("cdd"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("bbbbb"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("~!@#$%^&*()_+QWERTYUIOP{}ASDFGHJKL:ZXCVBNM<>?`1234567890-=qwertyuiop[]|asdfghjkl;'zxcvbnm,./"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("x"));
+            Console.WriteLine(solution.LengthOfLongestSubstring("dvdf"));
+
+
+
+
+        }
+    }
+}
---
+++ LongestSubstring/Solution.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LongestSubstring
+{
+    public class Solution
+    {
+        public int LengthOfLongestSubstring(string s)
+        {
+            int max = 0;
+            
+            // checking for null input
+            if (s.Length == 0)
+            {
+                max = 0;
+            }
+
+            //checking for one letter input
+            if (s.Length == 1)
+            {
+                max = 1;
+            }
+
+            string temp = "";
+            int[] output = new int[s.Length];
+            int index = 0;
+
+            for (int i = 0; i < s.Length; i++)
+            {
+                //if the letter is a repeat, delete up to repeat and then add it to temp, increase index
+                if (temp.Contains(s[i]))
+                {
+                    index++;
+                    int deleteIndex = temp.IndexOf(s[i])+1;
+                    temp = temp.Remove(0,deleteIndex);
+                }
+
+                temp += s[i];
+                output[index] = temp.Length;
+            }
+
+            if (s.Length > 1)
+                max = output.Max();
+
+            return max;
+        }
+    }
+}
